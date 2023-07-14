ebedf1bff2fadd1d691e888fd26475cb7c93f475
ten66 <ikesho.1217@gmail.com>
2023-07-14 22:51:05 +0900

    delete a task

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -55,6 +55,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // delete task
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,6 +88,7 @@ class _HomePageState extends State<HomePage> {
             taskName: toDoList[index][0],
             taskCompleted: toDoList[index][1],
             onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
           );
         },
         // children: [
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   // requiredとは? ->
   ToDoTile({
@@ -11,41 +13,55 @@ class ToDoTile extends StatelessWidget {
     required this.taskName,
     required this.taskCompleted,
     required this.onChanged,
+    required this.deleteFunction,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
-      child: Container(
-        padding: EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          // 角を取る
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            // checkbox -> flutterでWidgetとして用意されている
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              // [original]チェックボックスの色がわかりづらいため、オレンジに変更
-              activeColor: Colors.orange,
-            ),
-
-            // タスクの名前
-            Text(
-              taskName,
-              style: TextStyle(
-                // 三項演算子
-                // チェックボックスにチェックがついているかどうかでテキストに線を入れる
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade300,
+              borderRadius: BorderRadius.circular(12),
             ),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            // 角を取る
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              // checkbox -> flutterでWidgetとして用意されている
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                // [original]チェックボックスの色がわかりづらいため、オレンジに変更
+                activeColor: Colors.orange,
+              ),
+
+              // タスクの名前
+              Text(
+                taskName,
+                style: TextStyle(
+                  // 三項演算子
+                  // チェックボックスにチェックがついているかどうかでテキストに線を入れる
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
